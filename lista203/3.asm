de191bc5e8e5ec4e5f861c11fbd4aee5f9fcbf3d
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-05 21:37:34 -0300

    Update 3.asm

--- lista203/3.asm
+++ lista203/3.asm
@@ -3,9 +3,19 @@
 
 # a) 
 # B[0] = A[1];
+lw $t0, 4($s6)
+sw $t0, 0($s7)
 
 # b)
 # B[i] = A[j];
+sll $t0, $s4, 2
+add $t0, $t0, $s6 # &A[j]
+lw $t0, 0($t0)
+
+sll $t1, $s3, 2
+add $t1, $t1, $s7 # &B[i]
+
+sw $t0, 0($t1)
 
 # c)
 # B[i + 1] = A[j * 2];
