2c824b6a39a38fae0cc6ba7464efef6985612747
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-08 15:22:34 -0300

    Update exercicios.asm

--- aulas/aula11_08/exercicios.asm
+++ aulas/aula11_08/exercicios.asm
@@ -5,7 +5,7 @@ add $t2, $t2, $s2 # seta o edereço de D[i+j]
 lw $t3, 0($t2) # load D[i+j]
 
 WHILE: slt $t4, $t3, $s0 # D[i+j] < a
-bne $t4, $0, EXIT
+beq $t4, $0, EXIT
 add $t3, $t3, $s1 # D[i+j] += b
 sw $t3, 0($t2)
 j WHILE
@@ -14,8 +14,10 @@ EXIT:
 #2)
 and $t0, $0, $0
 FOR_i: slt $t2, $t1, $s0 # i < a = 1
+beq $t2, $0, EXIT
 and $t1, $0, $0
 FOR_j: slt $t3, $t1, $s1 # j < b = 1
+beq $t2, $s0, FOR_i
 add $t4, $t0, $t1 # i+j
 sll $t5, $t4, 2 # transforma i+j em desvio
 add $t5, $t4, $s2 # seta &D[i+j]
@@ -24,3 +26,4 @@ addi $t1, $t1, 1 # j++
 j FOR_j
 addi $t0, $t0, 1 # i++
 j FOR_i
+EXIT
