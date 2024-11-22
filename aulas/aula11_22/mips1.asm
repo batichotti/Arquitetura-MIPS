6a1b846708760001552322c4da703a4b041ebc86
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-22 14:41:35 -0300

    Update mips1.asm

--- aulas/aula11_22/mips1.asm
+++ aulas/aula11_22/mips1.asm
@@ -9,13 +9,20 @@
 SOMAVET:
 addi $sp, $sp, -8
 sw $s0, 0($sp)
-sw $s1, 4($s1)
+sw $s1, 4($s1) # salvando o estado antes da chamada
 
+and $s0, $0, $0 # ac = 0
+add $s1, $0, $0
 LOOP:
-
-
-j LOOP
+	slt $t0, $s1, $a1 # t0 = i < n
+	beq $t0, $0, EXIT # se i >= n : EXIT
+	sll $t1, $s1, 2 # t1 = i * 4
+	add $t1, $s1, $a0 # t1 = &vet[i]
+	lw $t2, 0($t1) # t2 = vet[i]
+	add $s0, $s0, $t2 # ac = ac + vet[i]
+	addi $s1, $s1, 1
+	j LOOP
 EXIT:
-lw $s0, 0($sp)
+lw $s0, 0($sp) # logando o estado depois da execução
 lw $s1, 4($s1)
 jr $ra
